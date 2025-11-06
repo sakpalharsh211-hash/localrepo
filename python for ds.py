68b4834d54d65f1153f2843bec55668ec522d02d
sakpalharsh211-hash <sakpalharsh211@gmail.com>
2025-11-06 12:51:30 +0530

    Delete python for ds.py

--- python for ds.py
+++
@@ -1,8 +0,0 @@
-# Creating nested tuples
-inner_tuple1 = (1, 2, 3)
-inner_tuple2 = ('a', 'b', 'c')
-outer_tuple = (inner_tuple1, inner_tuple2)
-
-# Display the nested tuple
-print("Nested Tuple:")
-print(outer_tuple)
