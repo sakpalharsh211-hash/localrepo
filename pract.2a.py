86ba3ab7329c7b24f4a87752c9da16e47f5a1cb3
sakpalharsh211-hash <sakpalharsh211@gmail.com>
2025-11-06 12:47:57 +0530

    Delete pract.2a.py

--- pract.2a.py
+++
@@ -1,7 +0,0 @@
-def fibonacci(n):
-    a,b=0,1
-    for i in range (n):
-        print (a,end="\n")
-        a,b=b,a+b
-numb = int(input("Enter the number of terms in your Fibonacci Series "))
-fibonacci(numb)
